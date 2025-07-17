1d28bc1849c8f9521e578b94ba2022b01a8131fa
Mario Aragao <marioestudos.nuvem@gmail.com>
2025-07-17 14:28:51 -0300

    logs

--- Trabalho3/logs.py
+++ Trabalho3/logs.py
@@ -1,6 +1,6 @@
 import logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%d/%m/%Y %H:%M:%S',
     filename='app.log',
